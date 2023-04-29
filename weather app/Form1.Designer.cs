6d5e620bafb143fef652e099d449d5a2a421f059
Maddayum22 <madvanslooten@gmail.com>
2023-04-29 14:48:54 +0200

    Added extra stats to UI

--- weather app/Form1.Designer.cs
+++ weather app/Form1.Designer.cs
@@ -28,25 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            weatherOutput = new TextBox();
             getWeather = new Button();
             label1 = new Label();
             weatherInput = new TextBox();
+            weatherPic = new PictureBox();
+            label2 = new Label();
+            temperatureOutput = new Label();
+            errorText = new Label();
+            weatherTypeOutput = new Label();
+            weatherType = new Label();
+            city = new Label();
+            ((System.ComponentModel.ISupportInitialize)weatherPic).BeginInit();
             SuspendLayout();
-            // 
-            // weatherOutput
-            // 
-            weatherOutput.Location = new Point(44, 83);
-            weatherOutput.Multiline = true;
-            weatherOutput.Name = "weatherOutput";
-            weatherOutput.Size = new Size(567, 284);
-            weatherOutput.TabIndex = 0;
             // 
             // getWeather
             // 
-            getWeather.Location = new Point(473, 38);
+            getWeather.FlatStyle = FlatStyle.System;
+            getWeather.ForeColor = Color.FromArgb(11, 49, 66);
+            getWeather.Location = new Point(592, 40);
+            getWeather.Margin = new Padding(4, 3, 4, 3);
             getWeather.Name = "getWeather";
-            getWeather.Size = new Size(138, 29);
+            getWeather.Size = new Size(173, 31);
             getWeather.TabIndex = 1;
             getWeather.Text = "Get Weather";
             getWeather.UseVisualStyleBackColor = true;
@@ -55,43 +57,132 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(44, 43);
+            label1.Location = new Point(55, 45);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(112, 18);
+            label1.Size = new Size(99, 19);
             label1.TabIndex = 2;
             label1.Text = "Enter a City:";
             label1.Click += label1_Click;
             // 
             // weatherInput
             // 
-            weatherInput.Location = new Point(162, 40);
+            weatherInput.Location = new Point(203, 42);
+            weatherInput.Margin = new Padding(4, 3, 4, 3);
             weatherInput.Name = "weatherInput";
-            weatherInput.Size = new Size(305, 25);
+            weatherInput.Size = new Size(380, 27);
             weatherInput.TabIndex = 3;
             weatherInput.TextChanged += weatherInput_TextChanged;
             // 
+            // weatherPic
+            // 
+            weatherPic.BorderStyle = BorderStyle.Fixed3D;
+            weatherPic.Location = new Point(55, 131);
+            weatherPic.Margin = new Padding(4, 3, 4, 3);
+            weatherPic.Name = "weatherPic";
+            weatherPic.Size = new Size(70, 70);
+            weatherPic.TabIndex = 4;
+            weatherPic.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 216);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 19);
+            label2.TabIndex = 5;
+            label2.Text = "Temperature:";
+            label2.Click += label2_Click;
+            // 
+            // temperatureOutput
+            // 
+            temperatureOutput.AutoSize = true;
+            temperatureOutput.Location = new Point(183, 216);
+            temperatureOutput.Margin = new Padding(4, 0, 4, 0);
+            temperatureOutput.Name = "temperatureOutput";
+            temperatureOutput.Size = new Size(0, 19);
+            temperatureOutput.TabIndex = 6;
+            // 
+            // errorText
+            // 
+            errorText.AutoSize = true;
+            errorText.ForeColor = Color.Red;
+            errorText.Location = new Point(55, 86);
+            errorText.Margin = new Padding(4, 0, 4, 0);
+            errorText.Name = "errorText";
+            errorText.Size = new Size(0, 19);
+            errorText.TabIndex = 7;
+            // 
+            // weatherTypeOutput
+            // 
+            weatherTypeOutput.AutoSize = true;
+            weatherTypeOutput.Location = new Point(183, 257);
+            weatherTypeOutput.Margin = new Padding(4, 0, 4, 0);
+            weatherTypeOutput.Name = "weatherTypeOutput";
+            weatherTypeOutput.Size = new Size(0, 19);
+            weatherTypeOutput.TabIndex = 9;
+            weatherTypeOutput.Click += label3_Click;
+            // 
+            // weatherType
+            // 
+            weatherType.AutoSize = true;
+            weatherType.Location = new Point(55, 257);
+            weatherType.Margin = new Padding(4, 0, 4, 0);
+            weatherType.Name = "weatherType";
+            weatherType.Size = new Size(78, 19);
+            weatherType.TabIndex = 8;
+            weatherType.Text = "Weather:";
+            weatherType.Click += label4_Click;
+            // 
+            // city
+            // 
+            city.AutoSize = true;
+            city.Font = new Font("Microsoft YaHei", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            city.Location = new Point(392, 86);
+            city.Name = "city";
+            city.Size = new Size(50, 27);
+            city.TabIndex = 10;
+            city.Text = "City";
+            city.Click += city_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 18F);
+            AutoScaleDimensions = new SizeF(10F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
-            ClientSize = new Size(664, 441);
+            BackColor = Color.FromArgb(11, 49, 66);
+            ClientSize = new Size(830, 466);
+            Controls.Add(city);
+            Controls.Add(weatherTypeOutput);
+            Controls.Add(weatherType);
+            Controls.Add(errorText);
+            Controls.Add(temperatureOutput);
+            Controls.Add(label2);
+            Controls.Add(weatherPic);
             Controls.Add(weatherInput);
             Controls.Add(label1);
             Controls.Add(getWeather);
-            Controls.Add(weatherOutput);
-            Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Font = new Font("Microsoft YaHei", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = SystemColors.ButtonHighlight;
+            Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
             Text = "Weather App";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)weatherPic).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TextBox weatherOutput;
         private Button getWeather;
         private Label label1;
         private TextBox weatherInput;
+        private PictureBox weatherPic;
+        private Label label2;
+        private Label temperatureOutput;
+        private Label errorText;
+        private Label weatherTypeOutput;
+        private Label weatherType;
+        private Label city;
     }
 }
--- weather app/Form1.cs
+++ weather app/Form1.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Windows.Forms;
+
 namespace weather_app
 {
     public partial class Form1 : Form
@@ -13,13 +16,22 @@ namespace weather_app
             var weatherClass = new getWeatherData();
             var weatherData = await weatherClass.GetWeatherAsync(query);
 
-            if (weatherData != null)
+            if (weatherData != null && query.Length > 0)
             {
-                weatherOutput.Text = weatherData.Current.Temperature.ToString();
+                city.Text = query;
+                temperatureOutput.Text = weatherData.Current?.Temperature.ToString();
+                weatherTypeOutput.Text = weatherData.Current?.Weather_descriptions[0];
+                using (var client = new WebClient())
+                {
+                    var url = weatherData.Current?.Weather_icons?[0];
+                    var stream = client.OpenRead(url!);
+                    var image = Image.FromStream(stream);
+                    weatherPic.Image = image;
+                }
             }
             else
             {
-                weatherOutput.Text = "Something went wrong, have you spelled the city name correctly?";
+                errorText.Text = "Something went wrong, have you spelled the city name correctly?";
             }
 
         }
@@ -30,6 +42,30 @@ namespace weather_app
         }
 
         private void weatherInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void city_Click(object sender, EventArgs e)
         {
 
         }
